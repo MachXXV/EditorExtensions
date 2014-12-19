7d79a39463bc4a769a895b00aa4073fe67542edd
MachXXV <machxxv@machxxv.com>
2014-12-18 23:38:20 -0500

    v2.0 beta
    Mostly functional with KSP v0.90 minus vertical snap

--- EditorExtensions/EditorExtensions.cs
+++ EditorExtensions/EditorExtensions.cs
@@ -7,30 +7,26 @@ using System.IO;
 
 namespace EditorExtensions
 {
-	[KSPAddon (KSPAddon.Startup.EditorAny, true)]
+	[KSPAddon (KSPAddon.Startup.EditorAny, false)]
 	public class EditorExtensions : MonoBehaviour
 	{
-		//new vars
-		string pluginDirectory;
-
 		#region member vars
-	
-		const string launchSiteName_LaunchPad = "LaunchPad";
-		const string launchSiteName_Runway = "Runway";
-		const string degreesSymbol = "\u00B0";
-		const string VABGameObjectName = "interior_vehicleassembly";
-		const string SPHGameObjectName = "xport_sph3";
-	
+
+		//current vars
+		string pluginDirectory;
+		KeyCode keyMapResetCamera = KeyCode.Space;
+		KeyCode keyMapSurfaceAttachment = KeyCode.T;
+		//look into loading game keymaps for applying alt+shift modifiers
+		const string degreesSymbol = "\u00B0";	
 		int _symmetryMode = 0;
 		int maxSymmetryMode = 99;
-		static float[] angle = { 0, 1, 5, 15, 30, 45, 60, 90 };
-	
-		//bool ignoreHotKeys = false;
-	
-		bool altKeyPressed;
-		bool shiftKeyPressed;
-	
-		bool inVAB = false;
+		static float[] angleSnapValues = { 0, 1, 5, 15, 30, 45, 60, 90 };
+
+		//old vars
+		//const string launchSiteName_LaunchPad = "LaunchPad";
+		//const string launchSiteName_Runway = "Runway";	
+		//bool ignoreHotKeys = false;	
+		//bool inVAB = false;
 	
 		EditorLogic editor;
 	
@@ -66,36 +62,27 @@ namespace EditorExtensions
 		/// </summary>
 		public EditorExtensions ()
 		{
-			//string assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-
-
 			//Get plugin's dll version
 			try {
-				//DebugMsg("Path exec assembly: " + Assembly.GetExecutingAssembly().Location);
-				//DebugMsg("Path assembly: " + Assembly.GetAssembly(typeof(EditorExtensions)).Location);
-
-				Assembly execAssembly = Assembly.GetExecutingAssembly();
-				string assemblyVersion = execAssembly.GetName().Version.ToString();
-				string filePath = execAssembly.Location;
-				pluginDirectory = Path.GetDirectoryName(execAssembly.Location);
+				Assembly execAssembly = Assembly.GetExecutingAssembly ();
+				string assemblyVersion = execAssembly.GetName ().Version.ToString ();
+				pluginDirectory = Path.GetDirectoryName (execAssembly.Location);
 
 				DebugMsg ("Initializing version " + assemblyVersion);
-				DebugMsg ("Current plugin directory: " + pluginDirectory);
 			} catch (Exception ex) {
 				DebugMsg ("Unable to get assembly version: " + ex.Message);
 			}
+		}
 
+		void InitConfig(){
 			PluginConfiguration cfg = PluginConfiguration.CreateForType<EditorExtensions> ();
 			try {
 				cfg.load ();
 
+				ConfigNode cfgNode = new ConfigNode ("settings");
+				cfgNode.AddValue ("nodeName", "nodeValue");
 
-
-
-				ConfigNode cfgNode = new ConfigNode("settings");
-				cfgNode.AddValue("nodeName", "nodeValue");
-
-				cfgNode.Save("EditorExtensions.cfg");
+				cfgNode.Save ("EditorExtensions.cfg");
 
 				//Check to see what is returned when config isnt there
 				if (cfg == null) {
@@ -139,50 +126,33 @@ namespace EditorExtensions
 			} catch (Exception ex) {
 				ErrorMsg ("Error getting config value: " + ex.Message);
 			}
-
-
-
-
-
-		}
-
-		private void InitConfig ()
-		{
-
 		}
 	
 		//Unity initialization call
 		public void Awake ()
 		{
-			//DontDestroyOnLoad(this);
 			DebugMsg ("Awake() initializing");
 	
 			editor = EditorLogic.fetch;
-	
-			//skipping this for 0.21
-			//AlterEditorSpace(editor);
 	
 			editor.symmetrySprite.Hide (true);
 			editor.mirrorSprite.Hide (true);
 	
 			//Rects for symmetry/angle snap labels
-			symmetryLabelRect = new Rect (143, Screen.height - 65, 57, 57);
-			angleSnapLabelRect = new Rect (201, Screen.height - 63, 46, 46);
+			symmetryLabelRect = new Rect (150, Screen.height - 65, 57, 57);
+			angleSnapLabelRect = new Rect (206, Screen.height - 63, 46, 46);
 	
 			InitStyles ();
 	
 			//Disable shortcut keys when ship name textarea has focus
-			//shipNameField gone in 0.21
-			//editor.shipNameField.commitOnLostFocus = true;
-			//editor.shipNameField.AddCommitDelegate((IKeyFocusable _) => { ignoreHotKeys = false; });
-			//editor.shipNameField.AddFocusDelegate((UITextField _) => { ignoreHotKeys = true; });
 		}
 
+		//Broken
+		const string VABGameObjectName = "interior_vehicleassembly";
+		const string SPHGameObjectName = "xport_sph3";
+
 		void AlterEditorSpace (EditorLogic editor)
-		{
-			//maxheight gone in 0.21
-			//editor.maxHeight = 2000;
-	
+		{	
 			// Modify cameras/available interior space
 			if (HighLogic.LoadedScene == GameScenes.EDITOR) {
 				DebugMsg ("Updating VAB dimensions and camera");
@@ -211,102 +181,96 @@ namespace EditorExtensions
 //			}
 		}
 
-		public void Update ()
-		{
+		bool altKeyDown;
+		bool shiftKeyDown;
 
-			//Idea: change root part. 
-	
-			//need to verify the EditorLogic state - do we need to fetch it every time?
-			editor = EditorLogic.fetch;
-			if (editor == null)
-				return;
-	
+		public void Update ()
+		{		
 			//if(ignoreHotKeys || editor.editorScreen != EditorLogic.EditorScreen.Parts)
 			//    return;
 	
 			//may need to go away from this and do explicit editor.editorType calls 
 			//inVAB = (editor.editorType == EditorLogic.EditorMode.VAB);
+
+			//look into fuel crossfeed toggle
 	
 			//check for the various alt/mod etc keypresses
-			altKeyPressed = Input.GetKey (KeyCode.LeftAlt) || Input.GetKey (KeyCode.RightAlt) || Input.GetKey (KeyCode.AltGr);
+			altKeyDown = Input.GetKey (KeyCode.LeftAlt) || Input.GetKey (KeyCode.RightAlt) || Input.GetKey (KeyCode.AltGr);
 			//check for shift key
-			shiftKeyPressed = Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift);
+			shiftKeyDown = Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift);
+
+			//look into skewing camera
 	
 			//Space - when no part is selected, reset camera
-			if (Input.GetKeyDown (KeyCode.Space)) {
-				if (HighLogic.LoadedScene == GameScenes.EDITOR) {
-					VABCamera VABcam = Camera.main.GetComponent<VABCamera> ();
-					//VABcam.camPitch = 0;
-					//VABcam.camHdg = 0;
-					VABcam.ResetCamera ();
-				}
-//				else if (HighLogic.LoadedScene == GameScenes.SPH)
-//				{
-//					SPHCamera SPHcam = Camera.main.GetComponent<SPHCamera>();
-//					SPHcam.camPitch = 0;
-//					SPHcam.camHdg = 0;
-//					//SPHcam.ResetCamera();
-//				}
+			if (Input.GetKeyDown (keyMapResetCamera) && !EditorLogic.SelectedPart) {
+				//if (HighLogic.LoadedSceneIsEditor) {
+				VABCamera VABcam = Camera.main.GetComponent<VABCamera> ();
+				VABcam.camPitch = 0;
+				VABcam.camHdg = 0;
+				//VABcam.ResetCamera ();
+
+				SPHCamera SPHcam = Camera.main.GetComponent<SPHCamera> ();
+				SPHcam.camPitch = 0;
+				SPHcam.camHdg = 0;
+				//SPHcam.ResetCamera();
+				//}
 			}
 	
-			// Alt+M - Toggle VAB/SPH editor mode (while staying in the same hangar)
-//			if (Input.GetKeyDown(KeyCode.Tab))
-//			{
-//				if (editor.editorType == EditorLogic.EditorMode.SPH){
-//					editor.editorType = EditorLogic.EditorMode.VAB;
-//					editor.launchSiteName = launchSiteName_LaunchPad;
-//					OSDMessage ("VAB/Launchpad Mode", 1);
+			//Broken, api doesnt respond
+			// V - Vertical alignment toggle
+//			if (Input.GetKeyDown (KeyCode.V)) {
+//				//DebugMsg ("Toggling vertical snap");
+//				GameSettings.VAB_ANGLE_SNAP_INCLUDE_VERTICAL ^= true;
+//	
+//				//if normal radial angle snap is currently off, vertical snap will have no effect unless it is re-enabled
+//				//automatically set aangle snap to minimum - some people thought vert snap was broken in this situation, the game doesn't appear to allow it
+//				if (GameSettings.VAB_USE_ANGLE_SNAP == false && GameSettings.VAB_ANGLE_SNAP_INCLUDE_VERTICAL == true) {
+//					DebugMsg ("Enabling angle snap to allow vertical snap to work");
+//					//angle snap needs be > 0, otherwise log is spammed with DivideByZero errors
+//					if (editor.srfAttachAngleSnap == 0)
+//						editor.srfAttachAngleSnap = 1;
+//					GameSettings.VAB_USE_ANGLE_SNAP = true;
 //				}
-//				else{
-//					editor.editorType = EditorLogic.EditorMode.SPH;
-//					editor.launchSiteName = launchSiteName_Runway;
-//					editor.symmetryMode = 1;
-//					OSDMessage ("SPH/Runway Mode", 1);
-//				}
+//				OSDMessage ("Vertical snap " + (GameSettings.VAB_ANGLE_SNAP_INCLUDE_VERTICAL ? "enabled" : "disabled"), 1);
+//				DebugMsg ("Vertical snap " + (GameSettings.VAB_ANGLE_SNAP_INCLUDE_VERTICAL ? "enabled" : "disabled"));
 //				return;
 //			}
 	
-			// V - Vertical alignment toggle
-			if (Input.GetKeyDown (KeyCode.V)) {
-				//DebugMsg ("Toggling vertical snap");
-				GameSettings.VAB_ANGLE_SNAP_INCLUDE_VERTICAL ^= true;
-	
-				//if normal radial angle snap is currently off, vertical snap will have no effect unless it is re-enabled
-				//automatically set aangle snap to minimum - some people thought vert snap was broken in this situation, the game doesn't appear to allow it
-				if (GameSettings.VAB_USE_ANGLE_SNAP == false && GameSettings.VAB_ANGLE_SNAP_INCLUDE_VERTICAL == true) {
-					DebugMsg ("Enabling angle snap to allow vertical snap to work");
-					//angle snap needs be > 0, otherwise log is spammed with DivideByZero errors
-					if (editor.srfAttachAngleSnap == 0)
-						editor.srfAttachAngleSnap = 1;
-					GameSettings.VAB_USE_ANGLE_SNAP = true;
-				}
-				OSDMessage ("Vertical snap " + (GameSettings.VAB_ANGLE_SNAP_INCLUDE_VERTICAL ? "enabled" : "disabled"), 1);
-				DebugMsg ("Vertical snap " + (GameSettings.VAB_ANGLE_SNAP_INCLUDE_VERTICAL ? "enabled" : "disabled"));
-				return;
-			}
-	
-			// ALT+R : Toggle radial/surface attachment globally
-			// When part is selected, also toggles surface attachment for that part, even if that part's cfg has it disabled.
-			if (altKeyPressed && Input.GetKeyDown (KeyCode.R)) {
-				if (EditorLogic.SelectedPart) {
-					//Toggle surface attachment for selected part, set global surface attachment toggle to match
-					EditorLogic.SelectedPart.attachRules.srfAttach ^= true;
-					editor.allowSrfAttachment = EditorLogic.SelectedPart.attachRules.srfAttach;
+			//look into also toggling  editor.allowNodeAttachment
+			//EditorLogic.SelectedPart.attachRules.allowStack
+
+			// T: Surface attachment and node attachment toggle
+			if (Input.GetKeyDown (keyMapSurfaceAttachment)) {
+
+				Part selectedPart = EditorLogic.SelectedPart;
+
+				if (selectedPart) {
+					//Toggle surface attachment for selected part
+					selectedPart.attachRules.srfAttach ^= true;
+
+					//set global toggling to match
+					editor.allowSrfAttachment = selectedPart.attachRules.srfAttach;
+					editor.allowNodeAttachment = !selectedPart.attachRules.srfAttach;
+
 					DebugMsg ("Toggling srfAttach for " + EditorLogic.SelectedPart.name);
-					OSDMessage (String.Format ("Surface attachment {0}, and for {1}"
-					                         , EditorLogic.SelectedPart.attachRules.srfAttach ? "enabled" : "disabled"
-					                         , EditorLogic.SelectedPart.name
+					OSDMessage (String.Format ("Surface attachment {0} \n Node attachment {1} \n for {2}"
+						, selectedPart.attachRules.srfAttach ? "enabled" : "disabled"
+						, editor.allowNodeAttachment ? "enabled" : "disabled"
+						, selectedPart.name
 					), 1);
 				} else {
 					//just toggle global surface attachment, parts whose config do not allow it are unaffected
 					editor.allowSrfAttachment ^= true;
-					OSDMessage ("Allow surface attachment is globally " + (editor.allowSrfAttachment ? "enabled" : "disabled"), 1);
-					DebugMsg ("Allow surface attachment is globally " + (editor.allowSrfAttachment ? "enabled" : "disabled"));
+					editor.allowNodeAttachment = !editor.allowSrfAttachment;
+					OSDMessage (String.Format ("Surface attachment {0} \n Node attachment {1}"
+						, editor.allowSrfAttachment ? "enabled" : "disabled"
+						, editor.allowNodeAttachment ? "enabled" : "disabled"
+					), 1);
 				}
 			}
 	
 			// ALT+Z : Toggle part clipping (From cheat options)
-			if (altKeyPressed && Input.GetKeyDown (KeyCode.Z)) {
+			if (altKeyDown && Input.GetKeyDown (KeyCode.Z)) {
 				CheatOptions.AllowPartClipping ^= true;
 				DebugMsg ("AllowPartClipping " + (CheatOptions.AllowPartClipping ? "enabled" : "disabled"));
 				OSDMessage ("Part clipping " + (CheatOptions.AllowPartClipping ? "enabled" : "disabled"), 1);
@@ -316,20 +280,20 @@ namespace EditorExtensions
 			// C, Shift+C : Increment/Decrement Angle snap
 			if (Input.GetKeyDown (KeyCode.C)) {
 	
-				if (!altKeyPressed) {
+				if (!altKeyDown) {
 					//GameSettings.VAB_USE_ANGLE_SNAP = false;
 					DebugMsg ("Starting srfAttachAngleSnap = " + editor.srfAttachAngleSnap.ToString ());
 	
-					int currentAngleIndex = Array.IndexOf (angle, editor.srfAttachAngleSnap);
+					int currentAngleIndex = Array.IndexOf (angleSnapValues, editor.srfAttachAngleSnap);
 	
 					DebugMsg ("currentAngleIndex: " + currentAngleIndex.ToString ());
 	
 					float newAngle;
-					if (shiftKeyPressed) {
-						newAngle = angle [currentAngleIndex == 0 ? angle.Length - 1 : currentAngleIndex - 1];
+					if (shiftKeyDown) {
+						newAngle = angleSnapValues [currentAngleIndex == 0 ? angleSnapValues.Length - 1 : currentAngleIndex - 1];
 					} else {
-						DebugMsg ("new AngleIndex: " + (currentAngleIndex == angle.Length - 1 ? 0 : currentAngleIndex + 1).ToString ());
-						newAngle = angle [currentAngleIndex == angle.Length - 1 ? 0 : currentAngleIndex + 1];
+						DebugMsg ("new AngleIndex: " + (currentAngleIndex == angleSnapValues.Length - 1 ? 0 : currentAngleIndex + 1).ToString ());
+						newAngle = angleSnapValues [currentAngleIndex == angleSnapValues.Length - 1 ? 0 : currentAngleIndex + 1];
 					}
 	
 					DebugMsg ("Setting srfAttachAngleSnap to " + newAngle.ToString ());
@@ -355,20 +319,27 @@ namespace EditorExtensions
 			}
 	
 			// X, Shift+X : Increment/decrement symmetry mode
-			if (inVAB && Input.GetKeyDown (KeyCode.X)) {
-				if (altKeyPressed || (_symmetryMode < 2 && shiftKeyPressed)) {
-					//Alt+X or Symmetry is at 1(index 2) or lower
-					_symmetryMode = 0;
-				} else if (_symmetryMode > maxSymmetryMode - 2 && !shiftKeyPressed) {
-					//Stop adding at max symmetry
-					_symmetryMode = maxSymmetryMode - 1;
+			if (Input.GetKeyDown (KeyCode.X)) {
+
+				//only inc/dec symmetry in radial mode, mirror is just 1&2
+				if (editor.symmetryMethod == SymmetryMethod.Radial) {
+					if (altKeyDown || (_symmetryMode < 2 && shiftKeyDown)) {
+						//Alt+X or Symmetry is at 1(index 2) or lower
+						_symmetryMode = 0;
+					} else if (_symmetryMode > maxSymmetryMode - 2 && !shiftKeyDown) {
+						//Stop adding at max symmetry
+						_symmetryMode = maxSymmetryMode - 1;
+					} else {
+						//inc/dec symmetry
+						_symmetryMode = _symmetryMode + (shiftKeyDown ? -1 : 1);
+					}
+					editor.symmetryMode = _symmetryMode;
+					DebugMsg ("Setting symmetry to " + _symmetryMode.ToString ());
 				} else {
-					//inc/dec symmetry
-					_symmetryMode = _symmetryMode + (shiftKeyPressed ? -1 : 1);
+					//editor.symmetryMethod == SymmetryMethod.Mirror
+					//update var with stock action's result
+					_symmetryMode = editor.symmetryMode;
 				}
-	
-				DebugMsg ("Setting symmetry to " + _symmetryMode.ToString ());
-				editor.symmetryMode = _symmetryMode;
 			}
 		}
 
@@ -401,13 +372,21 @@ namespace EditorExtensions
 		float messageCutoff = 0;
 		string messageText = "";
 
+		/// <summary>
+		/// Set a on screen display message
+		/// </summary>
+		/// <param name="message">Message string</param>
+		/// <param name="delay">Amount of time to display the message</param>
 		void OSDMessage (string message, float delay)
 		{
 			messageCutoff = Time.time + delay;
 			messageText = message;
-			DebugMsg (String.Format ("messageCutoff = {0}, messageText = {1}", messageCutoff.ToString (), messageText));
+			DebugMsg (String.Format ("OSD messageCutoff = {0}, messageText = {1}", messageCutoff.ToString (), messageText));
 		}
 
+		/// <summary>
+		/// check for On Screen Display message
+		/// </summary>
 		void DisplayOSD ()
 		{
 			if (Time.time < messageCutoff) {
@@ -427,31 +406,57 @@ namespace EditorExtensions
 		}
 		*/
 	
+		string symmetryLabelValue = string.Empty;
+
 		public void OnGUI ()
-		{
-			//need to verify the EditorLogic state - do we need to fetch it every time?
-			editor = EditorLogic.fetch;
-			if (editor == null)
-				return;
-	
+		{	
 			DisplayOSD ();
+
+			//Only show angle/symmetry sprites on parts tab
+			if (editor.editorScreen == EditorScreen.Parts) {
+
+				//symmetryLabelRect = new Rect (150, Screen.height - 65, 57, 57);
+				//angleSnapLabelRect = new Rect (206, Screen.height - 63, 46, 46);
+
+//				angleSnapLabelRect = new Rect () {
+//					xMin = 206,
+//					xMax = 206 + 46,
+//					yMin = Screen.height - 63,
+//					yMax = Screen.height - 63 + 46
+//				};
+
+				//need to shift labels in advanced mode to the right
+				if (EditorLogic.Mode == EditorLogic.EditorModes.ADVANCED) {
+					symmetryLabelRect.xMin = 184;
+					symmetryLabelRect.xMax = 184 + 57;
+					angleSnapLabelRect.xMin = 241;
+					angleSnapLabelRect.xMax = 241 + 46;
+				} else {
+					//EditorLogic.EditorModes.SIMPLE
+					symmetryLabelRect.xMin = 150;
+					symmetryLabelRect.xMax = 150 + 57;
+					angleSnapLabelRect.xMin = 207;
+					angleSnapLabelRect.xMax = 207 + 46;
+				}
+
+				//Radial mode number+R, mirror mode is M/MM
+				if (editor.symmetryMethod == SymmetryMethod.Radial) {
+					symmetryLabelValue = (editor.symmetryMode + 1) + "R";
+				} else if (editor.symmetryMethod == SymmetryMethod.Mirror) {
+					symmetryLabelValue = (editor.symmetryMode == 0) ? "M" : "MM";
+				}
+
+				// Show Symmetry level
+				GUI.Label (symmetryLabelRect, symmetryLabelValue, labelStyle);
 	
-			// Show Symmetry level
-			string sym = (editor.symmetryMode + 1) + "x";
-//	        if (editor.editorType == EditorLogic.EditorMode.SPH)
-//	            sym = (editor.symmetryMode == 0) ? "M" : "MM";
-	
-			GUI.Label (symmetryLabelRect, sym, labelStyle);
-	
-			// Show angle snap amount
-			editor.angleSnapSprite.Hide (GameSettings.VAB_USE_ANGLE_SNAP);
-			editor.symmetrySprite.Hide (true);
-	
-			//disable sprite to avoid out of bounds animation calls
-			//editor.symmetrySprite.enabled = false;
-	
-			if (GameSettings.VAB_USE_ANGLE_SNAP) {
-				GUI.Label (angleSnapLabelRect, editor.srfAttachAngleSnap + degreesSymbol, labelStyle);
+				// Show angle snap amount
+				editor.angleSnapSprite.Hide (GameSettings.VAB_USE_ANGLE_SNAP);
+				editor.symmetrySprite.Hide (true);
+
+				//show stock circle sprite when angle snap is off (0 degrees)
+				if (GameSettings.VAB_USE_ANGLE_SNAP) {
+					GUI.Label (angleSnapLabelRect, editor.srfAttachAngleSnap + degreesSymbol, labelStyle);
+				}
 			}
 		}
 
---
+++ ReadMe.md
@@ -0,0 +1,25 @@
+# Editor Extensions
+
+## Features
+
+*   Allows levels of symmetry from 1 to 50
+*   Adds rotational/angle snapping at 1,5,15,30,45,90 degrees.
+*   Toggle vertical snap mode.
+*   Toggle part clipping (Use with caution, from the cheat options)
+*   Toggle radial attachment of parts
+*   Toggle between SPH/Runway and VAB/Launchpad editor modes
+*   Reset hangar camera
+
+## Keybindings
+
+*   V			- Toggle vertical snap
+*   X, Shift+X	- Increase/Decrease symmetry level
+*   Alt+X		- Reset symmetry level
+*   C, Shift+C	- Increase/Decrease angle snap
+*   Alt+C		- Reset angle snap
+*   Alt+R		- Toggle radial/surface attachment. For all parts, and when a part is selected, will toggle surface attachment even when that part's config usually does not allow it
+*   Alt+Z		- Toggle part clipping (CAUTION: This is a cheat option)
+*   Space		- When no part is selected, resets camera pitch and heading (straight ahead and level)
+*   Tab			- Toggles between VAB/SPH editor modes, and toggles launching to runway or launchpad.
+
+Released under MIT license.
